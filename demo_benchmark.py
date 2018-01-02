118b5a2b54a2eb8daea4557faa8c5b25d9cbe6b2
rfouyang <rfouyang@gmail.com>
2018-01-02 15:06:37 +0800

    first

--- demo_benchmark.py
+++ demo_benchmark.py
@@ -2,8 +2,14 @@ import numpy as np
 from skopt import gp_minimize
 
 
-def func_square(x):
+def func_square_1d(x):
     x = np.array(x)
+    return (x.dot(x))
+
+
+def func_square_3d(x):
+    c = np.arange(3)
+    x = np.array(x) - c
     return (x.dot(x))
 
 
@@ -11,9 +17,9 @@ def callback(info):
     print(info['x'], info['fun'])
 
 
-def main():
-    res = gp_minimize(func_square,
-                      [(-10.0, 10.0)],
+def demo_1d():
+    res = gp_minimize(func_square_1d,
+                      [(-5.0, 5.0)],
                       base_estimator='GP',
                       n_calls=20,
                       n_random_starts=5,
@@ -23,12 +29,43 @@ def main():
                       callback=callback,
                       n_points=10000,
                       n_restarts_optimizer=5,
-                      xi=0.01,
+                      xi=0.1,
                       kappa=1.96,
                       noise="gaussian",
                       n_jobs=1)
 
     print(res['x'])
+
+
+def demo_3d():
+    dimensions = [
+        (-5.0, 5.0),
+        (-5.0, 5.0),
+        (-5.0, 5.0)
+    ]
+    res = gp_minimize(func_square_3d,
+                      dimensions,
+                      base_estimator='GP',
+                      n_calls=100,
+                      n_random_starts=30,
+                      acq_func="LCB",
+                      acq_optimizer="auto",
+                      verbose=False,
+                      callback=callback,
+                      n_points=10000,
+                      n_restarts_optimizer=100,
+                      xi=0.1,
+                      kappa=1.96,
+                      noise="gaussian",
+                      n_jobs=1)
+
+    print(res['x'])
+
+
+def main():
+    demo_1d()
+    demo_3d()
+
 
 
 if __name__=='__main__':
